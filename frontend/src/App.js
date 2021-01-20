18b4f36ecc8a324e5b6fdb351b9972ad4a4df919
rinaykumar <rkumar4@mail.sfsu.edu>
2021-01-19 18:29:56 -0800

    Random line images, cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './css/styles.css';
 import './css/bootstrap.min.css';
-import Header from './assets/img/header.jpg';
-import Line from './assets/img/line.jpg';
-import Timer from './assets/img/timer.gif';
-import Timer2 from './assets/img/timer2.gif';
 import { Modal } from 'react-bootstrap';
+// Images
+import Header from './assets/img/header.jpg';
 import Yup from './assets/img/yup2.gif';
 import Nope from './assets/img/nope1.gif';
+import Line from './assets/img/line-img/line.jpg';
+import Line2 from './assets/img/line-img/line2.jpeg';
+import Line3 from './assets/img/line-img/line3.jpg';
+import Line4 from './assets/img/line-img/line4.jpg';
+import Line5 from './assets/img/line-img/line5.jpg';
+import Line6 from './assets/img/line-img/line6.jpg';
+import Line7 from './assets/img/line-img/line7.jpg';
+import Line8 from './assets/img/line-img/line8.jpg';
+import Line9 from './assets/img/line-img/line9.jpg';
+import Line10 from './assets/img/line-img/line10.jpg';
+import Line11 from './assets/img/line-img/line11.jpg';
+import Line12 from './assets/img/line-img/line12.jpeg';
+import Akon from './assets/img/akon.jpg';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,6 +33,9 @@ class App extends React.Component {
       allLines: [' ', ' ', ' '],
       show: false,
       show2: false,
+      lineImg1: Line,
+      lineImg2: Line2,
+      lineImg3: Line3,
     };
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
@@ -57,6 +71,12 @@ class App extends React.Component {
     document.getElementById('background').style.filter = 'blur(0)';
   }
 
+  handleNext() {
+    this.hideModal();
+    this.hideModal2();
+    this.getLines();
+  }
+
   getLines() {
     fetch('/line')
       .then((res) => res.json())
@@ -71,7 +91,6 @@ class App extends React.Component {
     document.getElementById('blurdiv').style.filter = 'blur(0)';
     document.getElementById('top-part').style.filter = 'blur(5px)';
     document.getElementById('start-button').style.visibility = 'hidden';
-    //document.getElementById("skip-button").style.visibility = "visible";
     document.getElementById('lineBox').className += ' lineHover';
     document.getElementById('lineBox2').className += ' lineHover';
     document.getElementById('lineBox3').className += ' lineHover';
@@ -83,6 +102,132 @@ class App extends React.Component {
     this.interval = setInterval(() => {
       this.getLines();
     }, 10000);
+  }
+
+  setLineImg1() {
+    let num = Math.floor(Math.random() * 12);
+    num++;
+    switch (num) {
+      case 1:
+        this.setState({ lineImg1: Line });
+        break;
+      case 2:
+        this.setState({ lineImg1: Line2 });
+        break;
+      case 3:
+        this.setState({ lineImg1: Line3 });
+        break;
+      case 4:
+        this.setState({ lineImg1: Line4 });
+        break;
+      case 5:
+        this.setState({ lineImg1: Line5 });
+        break;
+      case 6:
+        this.setState({ lineImg1: Line6 });
+        break;
+      case 7:
+        this.setState({ lineImg1: Line7 });
+        break;
+      case 8:
+        this.setState({ lineImg1: Line8 });
+        break;
+      case 9:
+        this.setState({ lineImg1: Line9 });
+        break;
+      case 10:
+        this.setState({ lineImg1: Line10 });
+        break;
+      case 11:
+        this.setState({ lineImg1: Line11 });
+        break;
+      default:
+        this.setState({ lineImg1: Line12 });
+    }
+  }
+
+  setLineImg2() {
+    let num = Math.floor(Math.random() * 12);
+    num++;
+    switch (num) {
+      case 1:
+        this.setState({ lineImg2: Line });
+        break;
+      case 2:
+        this.setState({ lineImg2: Line2 });
+        break;
+      case 3:
+        this.setState({ lineImg2: Line3 });
+        break;
+      case 4:
+        this.setState({ lineImg2: Line4 });
+        break;
+      case 5:
+        this.setState({ lineImg2: Line5 });
+        break;
+      case 6:
+        this.setState({ lineImg2: Line6 });
+        break;
+      case 7:
+        this.setState({ lineImg2: Line7 });
+        break;
+      case 8:
+        this.setState({ lineImg2: Line8 });
+        break;
+      case 9:
+        this.setState({ lineImg2: Line9 });
+        break;
+      case 10:
+        this.setState({ lineImg2: Line10 });
+        break;
+      case 11:
+        this.setState({ lineImg2: Line11 });
+        break;
+      default:
+        this.setState({ lineImg2: Line12 });
+    }
+  }
+
+  setLineImg3() {
+    let num = Math.floor(Math.random() * 12);
+    num++;
+    switch (num) {
+      case 1:
+        this.setState({ lineImg3: Line });
+        break;
+      case 2:
+        this.setState({ lineImg3: Line2 });
+        break;
+      case 3:
+        this.setState({ lineImg3: Line3 });
+        break;
+      case 4:
+        this.setState({ lineImg3: Line4 });
+        break;
+      case 5:
+        this.setState({ lineImg3: Line5 });
+        break;
+      case 6:
+        this.setState({ lineImg3: Line6 });
+        break;
+      case 7:
+        this.setState({ lineImg3: Line7 });
+        break;
+      case 8:
+        this.setState({ lineImg3: Line8 });
+        break;
+      case 9:
+        this.setState({ lineImg3: Line9 });
+        break;
+      case 10:
+        this.setState({ lineImg3: Line10 });
+        break;
+      case 11:
+        this.setState({ lineImg3: Line11 });
+        break;
+      default:
+        this.setState({ lineImg3: Line12 });
+    }
   }
 
   setAllLines() {
@@ -100,19 +245,36 @@ class App extends React.Component {
         this.setState({ line2: data.random });
       });
 
-    if (num === 0) {
+    if (this.state.total === 2) {
       this.setState({
-        allLines: [this.state.fakeLine, this.state.line1, this.state.line2],
+        allLines: [
+          'Chain the color of Akon',
+          this.state.fakeLine,
+          this.state.line2,
+        ],
+        lineImg1: Akon,
       });
-    } else if (num === 1) {
-      this.setState({
-        allLines: [this.state.line1, this.state.fakeLine, this.state.line2],
-      });
-    } else if (num === 2) {
-      this.setState({
-        allLines: [this.state.line1, this.state.line2, this.state.fakeLine],
-      });
+    } else {
+      if (num === 0) {
+        this.setState({
+          allLines: [this.state.fakeLine, this.state.line1, this.state.line2],
+        });
+      } else if (num === 1) {
+        this.setState({
+          allLines: [this.state.line1, this.state.fakeLine, this.state.line2],
+        });
+      } else if (num === 2) {
+        this.setState({
+          allLines: [this.state.line1, this.state.line2, this.state.fakeLine],
+        });
+      }
     }
+
+    if (this.state.total !== 2) {
+      this.setLineImg1();
+    }
+    this.setLineImg2();
+    this.setLineImg3();
   }
 
   counter(value) {
@@ -121,6 +283,9 @@ class App extends React.Component {
     if (this.state.allLines[value] === this.state.fakeLine) {
       let cor = this.state.correct + 1;
       this.setState({ correct: cor });
+      this.showModal();
+    } else {
+      this.showModal2();
     }
 
     this.setState({ total: tot });
@@ -133,7 +298,7 @@ class App extends React.Component {
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    //clearInterval(this.interval);
   }
 
   render() {
@@ -190,8 +355,8 @@ class App extends React.Component {
               >
                 <img
                   className="rounded-circle d-flex justify-content-start"
-                  src={Line}
-                  height="72px"
+                  src={this.state.lineImg1}
+                  height="70px"
                 />
                 <p className="text-left line-text">
                   "{this.state.allLines[0]}"
@@ -204,8 +369,8 @@ class App extends React.Component {
               >
                 <img
                   className="rounded-circle d-flex justify-content-start"
-                  src={Line}
-                  height="72px"
+                  src={this.state.lineImg2}
+                  height="70px"
                 />
                 <p className="text-left line-text">
                   "{this.state.allLines[1]}"
@@ -218,8 +383,8 @@ class App extends React.Component {
               >
                 <img
                   className="rounded-circle d-flex justify-content-start"
-                  src={Line}
-                  height="72px"
+                  src={this.state.lineImg3}
+                  height="70px"
                 />
                 <p className="text-left line-text">
                   "{this.state.allLines[2]}"
@@ -238,25 +403,19 @@ class App extends React.Component {
             START
           </button>
         </div>
-        {/* About and Footer */}
-        {/* <div className="about-text about dflex text-center">
-        </div>
         <footer className="text-center footer">
           <p className="footer-text">
             ©&nbsp;2021 rinaykumar |&nbsp;
             <a href="https://github.com/rinaykumar/2chainz-or-not">GitHub</a>
           </p>
-        </footer> */}
-
-        <button
-          bsStyle="success"
-          className="btn-block"
-          onClick={this.showModal}
+        </footer> 
+        <Modal
+          show={this.state.show}
+          onHide={this.hideModal}
+          centered
+          backdrop="static"
+          keyboard={false}
         >
-          Yup
-        </button>
-
-        <Modal show={this.state.show} onHide={this.hideModal} centered backdrop="static" keyboard={ false }>
           <Modal.Body centered>
             <div className="text-center d-flex float-none justify-content-center align-items-center align-content-center align-self-center">
               <img
@@ -265,31 +424,26 @@ class App extends React.Component {
               />
               <h1 className="yup">YUP!</h1>
             </div>
-
             <br />
-
             <div className="text-center d-flex float-none justify-content-center align-items-center align-content-center align-self-center">
               <button
                 className="btn btn-dark btn-lg text-uppercase text-center rounded-pill"
                 id="next-button"
                 type="button"
+                onClick={() => this.handleNext()}
               >
                 NEXT
               </button>
             </div>
           </Modal.Body>
         </Modal>
-
-
-        <button
-          bsStyle="success"
-          className="btn-block"
-          onClick={this.showModal2}
+        <Modal
+          show={this.state.show2}
+          onHide={this.hideModal2}
+          centered
+          backdrop="static"
+          keyboard={false}
         >
-          Nope
-        </button>
-
-        <Modal show={this.state.show2} onHide={this.hideModal2} centered backdrop="static" keyboard={ false }>
           <Modal.Body centered>
             <div className="text-center d-flex float-none justify-content-center align-items-center align-content-center align-self-center">
               <img
@@ -298,24 +452,19 @@ class App extends React.Component {
               />
               <h1 className="yup">NOPE!</h1>
             </div>
-
             <br />
-
             <div className="text-center d-flex float-none justify-content-center align-items-center align-content-center align-self-center">
               <button
                 className="btn btn-dark btn-lg text-uppercase text-center rounded-pill"
                 id="next-button"
                 type="button"
+                onClick={() => this.handleNext()}
               >
                 NEXT
               </button>
             </div>
           </Modal.Body>
         </Modal>
-
-
-
-
       </div>
     );
   }
